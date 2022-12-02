be2d3f2bde7fabfbd3313bb1f44d97c340b86185
Jonas Büchel <jonas@frontify.com>
2022-12-02 10:56:34 +0100

    Exercise 1.1: Change countReducer to accept the step as the action

--- src/exercise/01.js
+++ src/exercise/01.js
@@ -3,16 +3,14 @@
 
 import * as React from 'react'
 
-function countReducer(state, newState) {
-  return newState
+function countReducer(count, step) {
+  return count + step
 }
 
 function Counter({initialCount = 0, step = 1}) {
-  // 🐨 replace React.useState with React.useReducer.
-  // 💰 React.useReducer(countReducer, initialCount)
-  const [count, setCount] = React.useReducer(countReducer, initialCount)
+  const [count, changeCount] = React.useReducer(countReducer, initialCount)
+  const increment = () => changeCount(step)
 
-  const increment = () => setCount(count + step)
   return <button onClick={increment}>{count}</button>
 }
 
